5c7a87cdfdeb1863a09d2a2661dd15f529daa99e
olgabot <olga.botvinnik@gmail.com>
2017-08-18 11:38:50 -0700

    pim init to make installable package

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = '1.0.0'
+
+required = open('requirements.txt').read().split('\n')
+
+setup(
+    name='dobby',
+    version=version,
+    description='Dobby is the heroic house-elf that automates SampleSheet generation from Google Sheets',
+    author='Olga Botvinnik',
+    author_email='olga.botvinnik@gmail.com',
+    url='https://github.com/czbiohub/dobby',
+    packages=['dobby'],
+    install_requires=required,
+    long_description='See ' + 'https://github.com/czbiohub/dobby',
+    license='MIT'
+)
