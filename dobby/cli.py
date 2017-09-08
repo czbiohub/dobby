e3d8e8ff0a84b8a90e3cd23503b10aabb8e40895
Paola Castro <paolacastropaolacastro@gmail.com>
2017-09-08 15:37:22 -0700

    mice picker

--- dobby/cli.py
+++ dobby/cli.py
@@ -2,6 +2,7 @@ import click
 
 from dobby.cherrypick import cherrypick
 from dobby.aggregate import aggregate
+from dobby.selector import selector
 
 settings = dict(help_option_names=['-h', '--help'])
 
@@ -10,14 +11,14 @@ settings = dict(help_option_names=['-h', '--help'])
              context_settings=settings)
 def cli():
     """
-    Hi! Dobby is the heroic house-elf that automates SampleSheet generation 
+    Hi! Dobby is the heroic house-elf that automates SampleSheet generation
     from Google Sheets. And, of course, Dobby is free.
     """
     pass
 
-
 cli.add_command(cherrypick)
 cli.add_command(aggregate)
+cli.add_command(selector)
 
 if __name__ == "__main__":
     cli()
