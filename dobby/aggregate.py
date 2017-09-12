d496dab3971da13f02a46c25a8c4c4ad05edb5aa
Paola Castro <paolacastropaolacastro@gmail.com>
2017-09-11 17:27:23 -0700

    write incomplete files

--- dobby/aggregate.py
+++ dobby/aggregate.py
@@ -59,7 +59,7 @@ def round_partial(value, resolution):
               help="Many machines can't produce volumes of any precision, so "
                    "this ensures that the final volumes are rounded to a "
                    "usable number")
-def aggregate(filenames, plate_size, output_folder, desired_concentration=0.5,
+def aggregate(filenames, plate_size, output_folder, desired_concentration,
               final_volume=400, round_volume_to=ROUND_VOLUME_TO):
     """Glue together cherrypick files by 384 samples for an ECHO pick list
 
@@ -69,11 +69,8 @@ def aggregate(filenames, plate_size, output_folder, desired_concentration=0.5,
     filenames : str
         Tidy files created by "dobby cherrypick" to aggregate
     """
-    mass = desired_concentration * final_volume
-
     aggregated = pd.DataFrame()
-    seen = []
-    i = 0
+    sheet_number = 0
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -81,67 +78,87 @@ def aggregate(filenames, plate_size, output_folder, desired_concentration=0.5,
     last_file = len(filenames) - 1
 
     for file_index, filename in enumerate(filenames):
-        seen.append(filename)
         df = pd.read_csv(filename)
         df = df.sort_values(['row_letter', 'column_number'])
 
-        end_row = plate_size-aggregated.shape[0]
-        to_add = df.iloc[:end_row]
-        to_keep = df.iloc[(end_row+1):]
-        aggregated = pd.concat([aggregated, to_add])
+        rows_filled = aggregated.shape[0]
+        end_row_index = plate_size - rows_filled
+        add_now = df.iloc[:end_row_index]
+        add_next_time = df.iloc[(end_row_index):] # this +1 should not be here
+        aggregated = pd.concat([aggregated, add_now])
 
+        is_last_file = file_index == last_file
+        is_complete_platesize = aggregated.shape[0] == plate_size
+        # end_on_incomplete_sheet = (file_index == last_file and add_next_time.shape[0] != 0)
+        if is_complete_platesize or is_last_file:
+            is_partial_sheet = (is_last_file and not is_complete_platesize)
+            formatted_echopick_sheet = format_for_echopick(
+                aggregated,
+                desired_concentration,
+                final_volume,
+                round_volume_to,
+                is_partial_sheet)
 
-        if aggregated.shape[0] == plate_size or file_index == last_file:
-            aggregated = aggregated.rename(
-                columns={"well": "Source well", 'concentration': 'C(ng/ul)',
-                         'plate': 'Plate number', 'name': "Name", })
-            aggregated['Desired C'] = desired_concentration
-            aggregated['Final V'] = final_volume
-            aggregated["Verdict"] = 1
-            aggregated['Type'] = 'Unknown'
-
-            aggregated['Combined name'] = aggregated['Name'] + '-' \
-                                          + aggregated['Verdict'].astype(str)
-
-            # In case the sample volume is bigger than the final volume, take
-            # the minimum
-            aggregated['Sample V'] = aggregated['C(ng/ul)'].map(
-                lambda x: min(mass/x, final_volume))
-            aggregated['Buffer V'] = final_volume - aggregated['Sample V']
-
-            aggregated['Rounded Sample V'] = aggregated['Sample V'].map(
-                lambda x: round_partial(x, round_volume_to))
-            aggregated['Rounded Buffer V'] = final_volume - \
-                                             aggregated['Rounded Sample V']
-
-            well_names = DESTINATIONS
-            if file_index == last_file:
-                last_index = len(aggregated['Rounded Buffer V'])
-                well_names = DESTINATIONS[:last_index]
-
-            aggregated['Destination well'] = well_names
-
-            # Reorder the columns
-            aggregated = aggregated[COLUMNS]
-
-            basename = 'echo_picklist_{}.csv'.format(str(i).zfill(5))
-
-            if file_index == last_file:
-                basename = 'echo_picklist_{}_incomplete.csv'.format(str(i).zfill(5))
-            csv = os.path.join(output_folder, basename)
-            aggregated.to_csv(csv, index=False)
-
-            click.echo('Wrote {n} files ({names}) to {csv}'.format(
-                n=len(seen), names=', '.join(seen), csv=csv))
-
+            write_csv_from_dataframe(formatted_echopick_sheet, sheet_number, output_folder, is_partial_sheet)
             # Increment the sheet number
-            i += 1
+            sheet_number += 1
 
             # Reset the filename keepers and growing datafarame
-            seen = [filename]
-            aggregated = to_keep
+            aggregated = add_next_time
 
-    if aggregated.shape[0] > 0:
-        click.echo("{n} samples from ({names}) didn't make it into a pick "
-                   "list "
-                   ":(".format(n=aggregated.shape[0], names=', '.join(seen)))
+            rows_leftover_from_lastfile = add_next_time.shape[0]
+            if is_last_file and rows_leftover_from_lastfile > 0:
+                formatted_echopick_sheet = format_for_echopick(
+                    aggregated,
+                    desired_concentration,
+                    final_volume,
+                    round_volume_to,
+                    True)
+
+                write_csv_from_dataframe(formatted_echopick_sheet, sheet_number, output_folder, True)
+
+
+def write_csv_from_dataframe(dataframe, sheet_number, output_folder, is_partial_sheet=False):
+    #generate_file
+    basename = 'echo_picklist_{}.csv'.format(str(sheet_number).zfill(5))
+    if is_partial_sheet:
+        basename = 'echo_picklist_{}_incomplete.csv'.format(str(sheet_number).zfill(5))
+    csv = os.path.join(output_folder, basename)
+    dataframe.to_csv(csv, index=False)
+
+
+def format_for_echopick(aggregated,desired_concentration,final_volume,round_volume_to,is_partial_sheet=False):
+    mass = desired_concentration * final_volume
+    aggregated = aggregated.rename(
+     columns={"well": "Source well", 'concentration': 'C(ng/ul)',
+              'plate': 'Plate number', 'name': "Name", })
+    aggregated['Desired C'] = desired_concentration
+    aggregated['Final V'] = final_volume
+    aggregated["Verdict"] = 1
+    aggregated['Type'] = 'Unknown'
+
+    aggregated['Combined name'] = aggregated['Name'] + '-' \
+                               + aggregated['Verdict'].astype(str)
+
+    # In case the sample volume is bigger than the final volume, take
+    # the minimum
+    aggregated['Sample V'] = aggregated['C(ng/ul)'].map(
+     lambda x: min(mass/x, final_volume))
+    aggregated['Buffer V'] = final_volume - aggregated['Sample V']
+
+    aggregated['Rounded Sample V'] = aggregated['Sample V'].map(
+     lambda x: round_partial(x, round_volume_to))
+    aggregated['Rounded Buffer V'] = final_volume - \
+                                  aggregated['Rounded Sample V']
+
+    well_names = DESTINATIONS
+    if is_partial_sheet: #TODO
+        last_index = len(aggregated['Rounded Buffer V'])
+        well_names = DESTINATIONS[:last_index]
+
+    aggregated['Destination well'] = well_names
+
+    # Reorder the columns
+    aggregated = aggregated[COLUMNS]
+
+    return aggregated
